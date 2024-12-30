3e493793279c1df095f7cf48053dbd3081d467bb
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-12-30 14:38:19 +0530

    render portfolio with constant

---
+++ constants/baners.js
@@ -0,0 +1,8 @@
+export const baners = {
+  title: "Baner Design",
+  images: [
+    "../assets/images/portfolio/Flyer.jpg",
+    "../assets/images/portfolio/logo/sparkware/Artboard 9.jpg",
+    "../assets/images/portfolio/logo/sparkware/Artboard 10.jpg",
+  ],
+};
---
+++ constants/handbills.js
@@ -0,0 +1,8 @@
+export const handbills = {
+  title: "Handbill Design",
+  images: [
+    "../assets/images/portfolio/Flyer.jpg",
+    "../assets/images/portfolio/logo/sparkware/Artboard 9.jpg",
+    "../assets/images/portfolio/logo/sparkware/Artboard 10.jpg",
+  ],
+};
