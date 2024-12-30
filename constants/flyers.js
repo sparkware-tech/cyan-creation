bd098bfbd1e8469049b0e58684baadb0e49a9342
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-12-30 15:26:05 +0530

    update image render

--- constants/flyers.js
+++ constants/flyers.js
@@ -2,6 +2,7 @@ export const flyers = {
   title: "Flyer design",
   images: [
     "../assets/images/portfolio/Flyer.jpg",
+    "../assets/images/portfolio/logo/sparkware/Artboard 11.jpg",
     "../assets/images/portfolio/logo/sparkware/Artboard 9.jpg",
     "../assets/images/portfolio/logo/sparkware/Artboard 10.jpg",
   ],
