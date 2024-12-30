7a9974800e1873e057e25c54b6c08bfe2ef0fe0f
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-12-30 21:17:27 +0530

    change file locations

--- assets/constants/logoes.js
+++
@@ -1,8 +0,0 @@
-export const logoes = {
-  title: "Logo Design",
-  images: [
-    "../assets/images/portfolio/logo/sprakware.jpg",
-    "../assets/images/portfolio/logo/sprakware.jpg",
-    "../assets/images/portfolio/logo/sprakware.jpg",
-  ],
-};
