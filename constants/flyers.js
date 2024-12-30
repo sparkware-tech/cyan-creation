4ba3a245d051f67f71541ca868b08dbcf1a93f9f
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2024-12-30 21:13:39 +0530

    change file location

--- constants/flyers.js
+++
@@ -1,9 +0,0 @@
-export const flyers = {
-  title: "Flyer design",
-  images: [
-    "../assets/images/portfolio/Flyer.jpg",
-    "../assets/images/portfolio/logo/sparkware/Artboard 11.jpg",
-    "../assets/images/portfolio/logo/sparkware/Artboard 9.jpg",
-    "../assets/images/portfolio/logo/sparkware/Artboard 10.jpg",
-  ],
-};
