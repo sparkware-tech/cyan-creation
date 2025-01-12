f2eabd3d42b726707f7c68ce49c24315404bcd45
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-01-12 13:22:47 +0530

    add handbills

--- constants/handbills.js
+++ constants/handbills.js
@@ -4,5 +4,7 @@ export const handbills = {
     "assets/images/portfolio/handbill/Akka ICT.jpg",
     "assets/images/portfolio/handbill/Dr ChemistryHanbill Preview.jpg",
     "assets/images/portfolio/handbill/Vidusha Hanbill Preview.jpg",
+    "assets/images/portfolio/handbill/High Speed.jpg",
+    "assets/images/portfolio/handbill/Kaulisha Hanbill Preview.jpg",
   ],
 };
