d341471775d5e5dc1f2ed94d528fa0326522face
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-01-12 10:28:06 +0530

    add baner

--- constants/baners.js
+++ constants/baners.js
@@ -2,5 +2,6 @@ export const baners = {
   title: "Baner Design",
   images: [
     "assets/images/portfolio/baner/de chem banner Preview.jpg",
+    "assets/images/portfolio/baner/Tharindu banner Preview.jpg",
   ],
 };
