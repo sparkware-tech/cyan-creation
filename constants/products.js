759840e47324dad05f6c644dbc7b369b77cdf06f
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-01-12 10:27:17 +0530

    add new prodcts

---
+++ constants/products.js
@@ -0,0 +1,8 @@
+export const products = {
+  title: "Product Design",
+  images: [
+    "assets/images/portfolio/product/Untitled-1.gif",
+    "assets/images/portfolio/product/Untitled-2.gif",
+    "assets/images/portfolio/product/Untitled-3.gif",
+  ],
+};
