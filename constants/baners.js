4ce56b1dbd62590fb1ee94fb22a4de5b84d861a0
Kavinda Deshan <100216231+tkdeshan@users.noreply.github.com>
2025-04-24 17:15:56 +0530

    update baner

--- constants/baners.js
+++ constants/baners.js
@@ -1,7 +1,11 @@
 export const baners = {
   title: "Baner Design",
   images: [
-    "assets/images/portfolio/baner/de chem banner Preview.jpg",
     "assets/images/portfolio/baner/Tharindu banner Preview.jpg",
+    "assets/images/portfolio/baner/Akka banner Preview.jpg",
+    "assets/images/portfolio/baner/de chem banner Preview.jpg",
+    "assets/images/portfolio/baner/Dr Chemistry oxfordbanner Preview.jpg",
+    "assets/images/portfolio/baner/meat shop banner Preview.jpg",
+    "assets/images/portfolio/baner/petalz banner Preview .jpg",
   ],
 };
